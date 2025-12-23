a37c25dc22adf2c6439395485c4d8c7d309c8916
reinaayde <ayde@med.umich.edu>
2025-12-23 13:54:11 -0500

    - Removing unnecessary/commented-out lines

--- src/zs_nac/training.py
+++ src/zs_nac/training.py
@@ -1,11 +1,9 @@
 import torch.optim as optim
-# from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import torch
 import torch.nn.functional as F
 from zs_nac.utils import *
 
-# torch.manual_seed(1)
 
 
 def training(
@@ -33,10 +31,8 @@ def training(
     ################################################################################################################
     # Creating train loaders
     ################################################################################################################
-    # doubly_noisy_img = add_noise(noisy_img, gamma, noise_std, noise_mean, n_chan, device)
     train_loader = create_loaders(
         noisy_img,
-        # doubly_noisy_img,
         batch_size,
     )
     ################################################################################################################
@@ -49,7 +45,7 @@ def training(
     # Start the training
     ################################################################################################################
     while epoch <= max_epochs and val_loss_tracker < stop_training:
-        # a = time.time()
+
         if n_chan == 1:
             training_loss, doubly_noisy_img, lr_noisier2noise = train(
                 network,
@@ -90,13 +86,8 @@ def training(
                 min_val_loss = new_val_loss
                 val_loss_tracker = 0
 
-        # writer.add_scalar("val_loss_noisier2noise", new_val_loss, epoch)
-        # writer.add_scalar("training_loss_noisier2noise", training_loss, epoch)
-        # writer.add_scalar("Lr_noisier2noise", lr_noisier2noise, epoch)
 
         epoch += 1
-        # b= time.time()
-        # print('epoch time {}'.format(b-a))
 
     return network, doubly_noisy_img, epoch
 
@@ -141,9 +132,6 @@ def train(network,
     avg_trn_cost = 0
     network.train(True)
     for ii, batch in enumerate(train_loader):
-        # (
-        #     noisy_input # doubly_noisy_input,
-        # ) = from_batch_to_devide(batch, device)
         noisy_input = batch
         doubly_noisy_input = add_noise(noisy_input, gamma, noise_std, noise_mean, n_chan, device)
         """Forward Path"""
@@ -164,13 +152,7 @@ def train(network,
         lr = get_lr(optimizer)
 
     return avg_trn_cost, doubly_noisy_input, lr
-    # loss = loss_func(noisy_img, doubly_noisy_img, network)
-    # optimizer.zero_grad()
-    # loss.backward()
-    # optimizer.step()
-    # scheduler.step(metrics=loss)
-    # lr = get_lr(optimizer)
-    # return loss.item(), lr
+
 def from_batch_to_devide(batch, device):
     return (ele.to(device).float() for ele in iter(batch[:]))
 
